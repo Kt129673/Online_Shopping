8b35dcfe38be2e0ae5ba5f86c7a52e1ce01ccd9d
KIRAN RAOSAHEB TILEKAR <kiran2tilekar@gmail.com>
2022-09-28 15:30:04 +0530

    My project setup completed sucessfully

---
+++ shoppingBackend/src/main/java/com/ecom/shoppingBackend/App.java
@@ -0,0 +1,13 @@
+package com.ecom.shoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
